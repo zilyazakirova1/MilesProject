3bf1fdbf9963560e7da0eb6da2a22408599505c9
Neto test <zilya.zakirova1991@gmail.com>
2023-03-24 14:30:44 +0300

    New

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
